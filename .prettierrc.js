006704ff88ee22b8da22841fc5701c86544460b8
Terrel Jones <terrel.jones@bounteous.com>
2022-09-16 11:44:40 -0700

    feat: added prettier, and configured to project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  importOrder: ['^react$', '<THIRD_PARTY_MODULES>'],
+  importOrderSortSpecifiers: true,
+};
