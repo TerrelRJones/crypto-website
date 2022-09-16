109077cd37c8627285a17ee26eb220767f72abe0
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-17 01:06:40 +0900

    feat: initial architecture and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx|json)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    preferAbsolute: true,
+    extensions: [".tsx", ".ts", ".js", ".json"],
+    alias: {
+      src: path.resolve("./src"),
+    },
+    modules: [path.resolve("./node_modules"), path.resolve("./src")],
+  },
+};
