994e7b0bbd9c7b8f571b8739a98f4d483a3de9c8
Jeric Hernandez <jeric.hernandez@bounteous.com>
2022-09-20 06:40:55 +0900

    refactor: change to glass box

--- src/components/GlassBox/GlassBox.test.tsx
+++ src/components/GlassBox/GlassBox.test.tsx
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import GlassBox from 'components/GlassBox';
 
 describe('GlassBox', () => {
   it('should render GlassBox', () => {
     render(<GlassBox />);
+    screen.debug();
   });
 });
