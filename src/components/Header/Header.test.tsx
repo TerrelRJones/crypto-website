d532e9195d8ebf6beade8c1c2333c865a0c43143
Terrel Jones <terrel.jones@bounteous.com>
2022-09-21 14:20:46 -0700

    chore: added test to every component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,8 @@
+import { Header } from './Header';
+import { render } from '@testing-library/react';
+
+describe('Header Component', () => {
+  it('should render Header', () => {
+    render(<Header />);
+  });
+});
